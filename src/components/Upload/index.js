6a69115e9408c87c9ee50e205ac9c829f88f334b
thiagoos16 <tos@icomp.ufam.edu.br>
2020-04-13 18:46:29 -0400

    add message upload component

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -5,21 +5,33 @@ import Dropzone from 'react-dropzone';
 import { DropContainer, UploadMessage } from './styles';
 
 export default class Upload extends Component {
-  render() {
-    return (
-        <Dropzone accept="image/*" onDropAccepted={() => {}}>
-            {/* usa um um partner do react remder props, que é uma forma de passar o children com um função em vez de usar um <Element/> */}
-            { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
-                <DropContainer
-                    { ... getRootProps() }
-                    isDragActive={isDragActive}
-                    isDragReject={isDragReject}
-                >
-                    <input { ... getInputProps()} />
-                    Arraste seu arquivo até aqui
-                </DropContainer>
-            )}
-        </Dropzone>
-    );
-  }
+    renderDragMessage = (isDragActive, isDragReject) => {
+        if (!isDragActive) {
+            return <UploadMessage>Arraste arquivos aqui ... </UploadMessage>;
+        }
+
+        if (isDragReject) {
+            return <UploadMessage type="error">Arquivo não suportado</UploadMessage>
+        }
+        
+        return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>
+    }
+
+    render() {
+        return (
+            <Dropzone accept="image/*" onDropAccepted={() => {}}>
+                {/* usa um um partner do react remder props, que é uma forma de passar o children com um função em vez de usar um <Element/> */}
+                { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+                    <DropContainer
+                        { ... getRootProps() }
+                        isDragActive={isDragActive}
+                        isDragReject={isDragReject}
+                    >
+                        <input { ... getInputProps()} />
+                        {this.renderDragMessage(isDragActive, isDragReject)}
+                    </DropContainer>
+                )}
+            </Dropzone>
+        );
+    }
 }
--- src/components/Upload/styles.js
+++ src/components/Upload/styles.js
@@ -21,4 +21,16 @@ export const DropContainer = styled.div.attrs({
   ${props => props.isDragReject && dragReject};
 `;
 
-export const UploadMessage = styled.p``;
+const messageColors = {
+  default: "#999",
+  error: "#e57878",
+  success: "#78e5d5"
+}
+
+export const UploadMessage = styled.p`
+  display: flex;
+  color: ${props => messageColors[props.type || "deafult"]};
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0;
+`;
