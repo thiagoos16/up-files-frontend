3c50df202adf1ecd98dfdc0f2ea4454c0e84990b
thiagoos16 <tos@icomp.ufam.edu.br>
2020-04-13 15:58:06 -0400

    add adjusts in main js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
+import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
+
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Container>
+        <Content> Teste </Content>
+        <GlobalStyle />
+      </Container>
+    );
   }
 }
 
