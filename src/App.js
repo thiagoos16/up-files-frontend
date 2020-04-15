c5c627c339a577acbb6cc83c262d6e3440023a42
thiagoos16 <tos@icomp.ufam.edu.br>
2020-04-14 21:36:46 -0400

    add state to files list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import { filesize } from 'filesize';
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -7,12 +9,38 @@ import Upload from "./components/Upload";
 import FileList from "./components/FileList";
 
 class App extends Component {
+  state = {
+    uploadedFiles: [],
+  };
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles) 
+    })
+  };
+
   render() {
+    const { uploadedFiles } =  this.state;
+    
     return (
       <Container>
         <Content>
-          <Upload />
-          <FileList />
+          <Upload onUpload={this.handleUpload} />
+          { !!uploadedFiles.length && (
+            <FileList files={uploadedFiles} />
+          ) }
         </Content>
         <GlobalStyle />
       </Container>
