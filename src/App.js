5fbb6706571a034a299987f2fb638ac66c690c1d
thiagoos16 <tos@icomp.ufam.edu.br>
2020-04-14 22:52:45 -0400

    add post file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { uniqueId } from 'lodash';
 import filesize from 'filesize';
 
+import api from "./services/api";
+
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
 
 import Upload from "./components/Upload";
 import FileList from "./components/FileList";
+import { MdReport } from 'react-icons/md';
 
 class App extends Component {
   state = {
@@ -29,6 +32,40 @@ class App extends Component {
     this.setState({
       uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles) 
     })
+
+    uploadedFiles.forEach(this.processUpload);
+  };
+
+  updateFile = (id, data) => {
+    this.setState({ uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+      return id === uploadedFile.id ? { ... uploadedFile, ... data } : uploadedFile;
+    }) })
+  }
+
+  processUpload = (uploadedFiles) => {
+    const data = new FormData();
+
+    data.append('file', uploadedFile.file, uploadedFile.name);
+
+    api.post('post', data, {
+      onUploadProgress: e => {
+        const progress =  parseInt(Math.round((e.loaded * 100) / e.total));
+
+        this.updateFile(uploadedFile.id, {
+          progress,
+        })
+      }
+    }).then(response => {
+      this.updateFile(uploadedFile.id, {
+        uploaded: true,
+        id: response.data._id,
+        url: response.data.url
+      });
+    }).catch( () => {
+      this.updateFile(uploadedFile.id, {
+        error: true
+      });
+    });
   };
 
   render() {
